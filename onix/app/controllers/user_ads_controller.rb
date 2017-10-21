4c6559032b5eab6a53ce59b6a555443b6de3ad6f
Hironori Akaishi <ah.gm3622@gmail.com>
2017-10-21 15:15:39 +0900

    user_ads作成

---
+++ onix/app/controllers/user_ads_controller.rb
@@ -0,0 +1,5 @@
+class UserAdsController < ApplicationController
+  def index
+
+  end
+end
--- onix/app/models/user_category.rb
+++ onix/app/models/user_category.rb
@@ -1,2 +1,4 @@
 class UserCategory < ApplicationRecord
+  belongs_to  :client
+  belongs_to  :category
 end
