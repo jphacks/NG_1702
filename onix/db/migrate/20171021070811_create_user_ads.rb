8b7ade2abd8b55d88ac795f68cf4e3c607a86929
Hironori Akaishi <ah.gm3622@gmail.com>
2017-10-21 18:48:55 +0900

    migrate

--- onix/db/migrate/20171021070811_create_user_ads.rb
+++ onix/db/migrate/20171021070811_create_user_ads.rb
@@ -2,7 +2,7 @@ class CreateUserAds < ActiveRecord::Migration[5.1]
   def change
     create_table :user_ads do |t|
       t.integer :user_id
-      t.integer :ad_id
+      t.integer :adra_id
       t.timestamps
     end
   end
